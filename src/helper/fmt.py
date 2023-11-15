b48f50bec2c88bd24f5ed80f5f0819b1a2812f7a
ThomasByr <tbouyer2000@gmail.com>
2023-11-16 00:43:54 +0100

    not gracefull stop (but at least enables cursor)

--- src/helper/fmt.py
+++ src/helper/fmt.py
@@ -1,11 +1,45 @@
 import logging
+import os
 import sys
+import signal
 from typing import Any
 
 from termcolor import colored
 from typing_extensions import override
 
 __all__ = ["UsefulFormatter", "UselessHandler"]
+
+
+if os.name == "nt":
+    import msvcrt  # type: ignore # noqa
+    import ctypes  # type: ignore # noqa
+
+    class __CursorInfo(ctypes.Structure):
+        _fields_ = [("size", ctypes.c_int), ("visible", ctypes.c_byte)]
+
+
+def hide_cursor():
+    if os.name == "nt":
+        ci = __CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = False
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif os.name == "posix":
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+
+def show_cursor():
+    if os.name == "nt":
+        ci = __CursorInfo()
+        handle = ctypes.windll.kernel32.GetStdHandle(-11)
+        ctypes.windll.kernel32.GetConsoleCursorInfo(handle, ctypes.byref(ci))
+        ci.visible = True
+        ctypes.windll.kernel32.SetConsoleCursorInfo(handle, ctypes.byref(ci))
+    elif os.name == "posix":
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
 
 
 def formatter(
@@ -52,4 +86,5 @@ class UselessHandler(logging.StreamHandler):
     def emit(self, record: logging.LogRecord) -> None:
         super().emit(record)
         if record.levelno >= logging.CRITICAL:  # exit on critical errors
-            sys.exit(1)
+            show_cursor()
+            os.kill(os.getpid(), signal.SIGTERM)
