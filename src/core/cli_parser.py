818106fda1ea00e5243f960aaa76cf671bbd130e
ThomasByr <tbouyer2000@gmail.com>
2023-11-17 09:58:15 +0100

    slightly better parser

--- src/core/cli_parser.py
+++ src/core/cli_parser.py
@@ -1,5 +1,8 @@
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentParser, FileType, Namespace
 from dataclasses import dataclass
+from typing import Any
+
+from typing_extensions import override
 
 from ..version import __version__
 
@@ -8,11 +11,32 @@ __all__ = ["Args", "parser", "check_args"]
 
 @dataclass
 class Args:
-    cfg_path: str
-    debug: bool
-    usernames: str
-    passwords: str
-    it_comb: tuple[int, int]
+    cfg_path: str = ".cfg"
+    debug: bool = False
+    usernames: str = "assets/users.txt"
+    passwords: str = "assets/passwd.txt"
+    it_comb: tuple[int, int] = (3, 2)
+    each: int = 1000
+
+
+class WeakParser(ArgumentParser):
+    @override
+    def add_argument(self, *args: Any, **kwargs: Any) -> "WeakParser":
+        kwargs["default"] = None
+        super().add_argument(*args, **kwargs)
+        return self
+
+    def add_path_argument(self, *args: Any, **kwargs: Any) -> "WeakParser":
+        kwargs["metavar"] = "P"
+        kwargs["type"] = FileType("r")
+        super().add_argument(*args, **kwargs)
+        return self
+
+    def add_int_argument(self, *args: Any, **kwargs: Any) -> "WeakParser":
+        kwargs["metavar"] = "N"
+        kwargs["type"] = int
+        super().add_argument(*args, **kwargs)
+        return self
 
 
 def parse_it_comb(it_comb: str) -> tuple[int, int]:
@@ -26,62 +50,79 @@ def parse_it_comb(it_comb: str) -> tuple[int, int]:
     raise ValueError("invalid it_comb value")
 
 
-def parser() -> Namespace:
-    parser = ArgumentParser(
+def parser() -> WeakParser:
+    parser = WeakParser(
         description="A simple CLI to bruteforce login forms",
         epilog="visit us on GitHub : https://github.com/ThomasByr/tor-brute",
     )
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"%(prog)s {__version__}",
+    return (
+        parser.add_argument(
+            "-v",
+            "--version",
+            action="version",
+            version=f"%(prog)s {__version__}",
+        )
+        .add_path_argument(
+            "-c",
+            "--config",
+            dest="cfg_path",
+            help="Path to the config file",
+        )
+        .add_path_argument(
+            "-u",
+            "--user",
+            dest="usernames",
+            help="Path to the usernames text file",
+        )
+        .add_path_argument(
+            "-p",
+            "--passwd",
+            dest="passwords",
+            help="Path to the passwords text file",
+        )
+        .add_argument(
+            "-i",
+            "--iter",
+            dest="it_comb",
+            metavar="N[, M]",
+            type=parse_it_comb,
+            help="how much to iterate over the combinations (default: 3, 2 for user, passwd)",
+        )
+        .add_int_argument(
+            "-e",
+            "--each",
+            dest="each",
+            help="each how many requests to change the Tor identity (default: 1000)",
+        )
+        .add_argument(
+            "-d",
+            "--debug",
+            dest="debug",
+            action="store_true",
+            help="Enable debug mode",
+        )
     )
-    parser.add_argument(
-        "-c",
-        "--config",
-        dest="cfg_path",
-        default=None,
-        help="Path to the config file",
-    )
-    parser.add_argument(
-        "-u",
-        "--users",
-        dest="usernames",
-        default=None,
-        help="Path to the usernames text file",
-    )
-    parser.add_argument(
-        "-p",
-        "--passwd",
-        dest="passwords",
-        default=None,
-        help="Path to the passwords text file",
-    )
-    parser.add_argument(
-        "-i",
-        "--iter",
-        dest="it_comb",
-        default=None,
-        type=parse_it_comb,
-        help="how much to iterate over the combinations (default: 3, 2 for user, passwd)",
-    )
-    parser.add_argument(
-        "-d",
-        "--debug",
-        dest="debug",
-        action="store_true",
-        help="Enable debug mode",
-    )
-
-    return parser.parse_args()
 
 
 def check_args(args: Namespace) -> Args:
-    return Args(
-        cfg_path=args.cfg_path or ".cfg",
-        debug=args.debug or False,
-        usernames=args.usernames or "assets/users.txt",
-        passwords=args.passwords or "assets/passwd.txt",
-        it_comb=args.it_comb or (3, 2),
-    )
+    a = Args()
+
+    # paths (and read property) are checked by the ArgParser
+    if args.cfg_path is not None:
+        a.cfg_path = args.cfg_path
+    if args.usernames is not None:
+        a.usernames = args.usernames
+    if args.passwords is not None:
+        a.passwords = args.passwords
+
+    if args.it_comb is not None:
+        if any(i < 1 for i in args.it_comb):
+            raise ValueError("it_comb values must be >= 1")
+        a.it_comb = args.it_comb
+    if args.each is not None:
+        if args.each < 10:
+            raise ValueError("each value must be at least >= 10")
+        a.each = args.each
+    if args.debug:
+        a.debug = args.debug
+    return a
