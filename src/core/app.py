99594799983d4f1b5340492712f4ecfdb3f8dd86
ThomasByr <tbouyer2000@gmail.com>
2023-11-17 16:16:08 +0100

    whole new mechanism

--- src/core/app.py
+++ src/core/app.py
@@ -9,7 +9,7 @@ import requests
 from requests.exceptions import ConnectionError, Timeout
 from alive_progress import alive_bar
 
-from ..generator import PasswdGenerator
+from ..generator import PasswdGenerator, TupleGenerator
 from ..onion import TorProxy
 from .cli_parser import Args
 
@@ -23,6 +23,10 @@ class App:
     def __init__(self, args: Args) -> None:
         self.consecutive_fails = 0
         self.lock = Lock()
+
+        self.count = 0
+        self.count_lock = Lock()
+
         self.logger = logging.getLogger(".".join(__name__.split(".")[1:]))
         self.cfg = ConfigParser()
         self.cfg.read(args.cfg_path)
@@ -37,6 +41,7 @@ class App:
         self.username_path = args.usernames
         self.password_path = args.passwords
         self.it_comb = args.it_comb
+        self.each = args.each
 
     def post_search(self, username_field: str, password_field: str) -> bool:
         self.session.cookies.clear_session_cookies()  # todo: benchmark against .clear()
@@ -90,11 +95,45 @@ class App:
                 password_field,
             )
 
+    def bar(self, bar, digits: int, user_count: int, passwd_count: int, /):
+        should_update_title = False
+        with self.count_lock:
+            self.count += 1
+            if self.count % passwd_count == 0:
+                should_update_title = True
+
+        if should_update_title:
+            bar.title(self.bar_title(self.count // passwd_count, digits, user_count))
+        try:
+            bar()
+        except RuntimeError:
+            # RuntimeError: dictionary changed size during iteration
+            #
+            # This is because of the `alive_bar` package
+            # which uses a dict internally to store the
+            # progress bar state.
+            #
+            # This error is raised when the progress bar
+            # is updated while the bar is being rendered.
+            #
+            # This is not a problem because the progress
+            # bar is only used to display the progress
+            # and not to store the state.
+            #
+            # So we just ignore this error.
+            pass
+
+    @staticmethod
+    def bar_title(i, digits, user_count, /) -> str:
+        return f"on user {(i+1):0{digits}}/{user_count}"
+
     def run(self):
         user = PasswdGenerator(self.username_path, self.it_comb[0])
         passwd = PasswdGenerator(self.password_path, self.it_comb[1])
+        g = TupleGenerator(user, passwd)
 
         digits = len(str(user.count))  # number of digits in user count
+        length = len(self.bar_title(0, digits, user.count))
 
         self.logger.info("Starting Tor 🧄 session ...")
         with TorProxy() as tor:
@@ -102,33 +141,31 @@ class App:
                 "http": f"socks5h://localhost:{tor.port}",  # use Tor for HTTP connections
             }
 
-            with alive_bar(total=user.count * passwd.count) as bar:
+            with alive_bar(total=g.count) as bar:
                 with ThreadPool(self.N_PROCESS) as pool:
-                    for i, u in enumerate(user()):
-                        bar.title(f"on user {(i+1):0{digits}}/{user.count}")
-                        for _ in pool.imap_unordered(
-                            lambda p: self.post_search(u, p[:]),  # noqa
-                            passwd(),  # todo: faster gen after 1st time
-                        ):
-                            try:
-                                bar()
-                            except RuntimeError:
-                                # RuntimeError: dictionary changed size during iteration
-                                #
-                                # This is because of the `alive_bar` package
-                                # which uses a dict internally to store the
-                                # progress bar state.
-                                #
-                                # This error is raised when the progress bar
-                                # is updated while the bar is being rendered.
-                                #
-                                # This is not a problem because the progress
-                                # bar is only used to display the progress
-                                # and not to store the state.
-                                #
-                                # So we just ignore this error.
-                                pass
+                    for i, u in enumerate(g()):
+                        if i % self.each == 0:
+                            pool.close()
+                            pool.join()
+                            bar.title(f"ID Swap ♻️{' '*(length-9)}")
+                            tor.identity_swap()
+                            # alternative would be to manually terminate and create a new pool
+                            # without the "with" statement
+                            pool.__init__(self.N_PROCESS)
+                            bar.title(
+                                self.bar_title(
+                                    self.count // passwd.count, digits, user.count
+                                )
+                            )
 
-                        tor.identity_swap()
+                        pool.apply_async(
+                            self.post_search,
+                            args=(u[0], u[1]),
+                            callback=lambda _: self.bar(
+                                bar, digits, user.count, passwd.count
+                            ),
+                        )
+                    pool.close()
+                    pool.join()
 
         self.logger.info("Done ✅")
